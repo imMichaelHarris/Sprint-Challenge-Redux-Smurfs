91de31985011cfdbe21a704a7690d0fc07ba9542
Michael Harris <mhguitar1997@gmail.com>
2019-06-21 09:05:58 -0700

    Create Smurf component

---
+++ smurfs/src/components/Smurf.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Smurf = props => {
+    return (
+        <div>
+            <h2>Name</h2>
+            <p>Age</p>
+            <p>Height</p>
+        </div>
+    )
+}
+
+export default Smurf;
