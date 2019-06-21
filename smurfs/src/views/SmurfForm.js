cb0b02b10b3e6ea6a394c4c557d03abe3c3ac512
Michael Harris <mhguitar1997@gmail.com>
2019-06-21 09:15:29 -0700

    Input fields add to form

--- smurfs/src/views/SmurfForm.js
+++ smurfs/src/views/SmurfForm.js
@@ -12,7 +12,10 @@ class SmurfForm extends React.Component {
     render(){
         <div>
             <form>
-                <input type="text" name="name" value={this.state.name} onChange={this.handleChanges} />
+                <input type="text" name="name" value={this.state.newSmurf.name} onChange={this.handleChanges} />
+                <input type="number" name="age" value={this.state.newSmurf.age} onChange={this.handleChanges} />
+                <input type="number" name="height" value={this.state.newSmurf.height} onChange={this.handleChanges} />
+
             </form>
         </div>
     }
