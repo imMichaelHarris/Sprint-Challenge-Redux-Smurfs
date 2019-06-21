f9814285dd97aff594a326e4c2868db31fc3b606
Michael Harris <mhguitar1997@gmail.com>
2019-06-21 09:16:26 -0700

    Label add to form

--- smurfs/src/views/SmurfForm.js
+++ smurfs/src/views/SmurfForm.js
@@ -1,22 +1,41 @@
-import React from 'react'
+import React from "react";
 
 class SmurfForm extends React.Component {
-    state = {
-        newSmurf: {
-            name: '',
-            age: '',
-            height: ''
-        }
+  state = {
+    newSmurf: {
+      name: "",
+      age: "",
+      height: ""
     }
+  };
 
-    render(){
-        <div>
-            <form>
-                <input type="text" name="name" value={this.state.newSmurf.name} onChange={this.handleChanges} />
-                <input type="number" name="age" value={this.state.newSmurf.age} onChange={this.handleChanges} />
-                <input type="number" name="height" value={this.state.newSmurf.height} onChange={this.handleChanges} />
+  render() {
+    <div>
+      <form>
+        <label>Name:</label>
+        <input
+          type="text"
+          name="name"
+          value={this.state.newSmurf.name}
+          onChange={this.handleChanges}
+        />
+        <label>Age:</label>
 
-            </form>
-        </div>
-    }
+        <input
+          type="number"
+          name="age"
+          value={this.state.newSmurf.age}
+          onChange={this.handleChanges}
+        />
+        <label>Height:</label>
+
+        <input
+          type="number"
+          name="height"
+          value={this.state.newSmurf.height}
+          onChange={this.handleChanges}
+        />
+      </form>
+    </div>;
+  }
 }
