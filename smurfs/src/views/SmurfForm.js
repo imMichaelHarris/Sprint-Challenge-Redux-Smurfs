f5e0e9bae11fa6d9af9dabc255a48528486d2116
Michael Harris <mhguitar1997@gmail.com>
2019-06-21 19:42:52 -0700

    MapStateToProps function add in smurfForm

--- smurfs/src/views/SmurfForm.js
+++ smurfs/src/views/SmurfForm.js
@@ -1,6 +1,6 @@
 import React from "react";
-import {connect} from 'react-redux';
-import {addSmurfToDb} from '../actions'
+import { connect } from "react-redux";
+import { addSmurfToDb } from "../actions";
 
 class SmurfForm extends React.Component {
   state = {
@@ -12,52 +12,60 @@ class SmurfForm extends React.Component {
   };
 
   handleChanges = e => {
-      this.setState({
-          newSmurf: {
-              ...this.state.newSmurf,
-              [e.target.name]: e.target.value
-          }
-      })
-  }
+    this.setState({
+      newSmurf: {
+        ...this.state.newSmurf,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
 
   addSmurf = e => {
-    e.preventDefault()
-    this.props.addSmurfToDb(this.state.newSmurf)
-  }
+    e.preventDefault();
+    this.props.addSmurfToDb(this.state.newSmurf);
+  };
 
   render() {
     return (
+      <div>
+        <form onSubmit={this.addSmurf}>
+          <label>Name:</label>
+          <input
+            type="text"
+            name="name"
+            value={this.state.newSmurf.name}
+            onChange={this.handleChanges}
+          />
+          <label>Age:</label>
 
-    <div>
-      <form onSubmit={this.addSmurf}>
-        <label>Name:</label>
-        <input
-          type="text"
-          name="name"
-          value={this.state.newSmurf.name}
-          onChange={this.handleChanges}
-        />
-        <label>Age:</label>
+          <input
+            type="number"
+            name="age"
+            value={this.state.newSmurf.age}
+            onChange={this.handleChanges}
+          />
+          <label>Height:</label>
 
-        <input
-          type="number"
-          name="age"
-          value={this.state.newSmurf.age}
-          onChange={this.handleChanges}
-        />
-        <label>Height:</label>
-
-        <input
-          type="number"
-          name="height"
-          value={this.state.newSmurf.height}
-          onChange={this.handleChanges}
-        />
-        <button>Add Smurf</button>
-      </form>
-    </div>
-    )
+          <input
+            type="number"
+            name="height"
+            value={this.state.newSmurf.height}
+            onChange={this.handleChanges}
+          />
+          <button>Add Smurf</button>
+        </form>
+      </div>
+    );
   }
 }
 
-export default connect(null, {addSmurfToDb})(SmurfForm);
+const mapStateToProps = state => {
+  return {
+    newSmurf: state.formData
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { addSmurfToDb }
+)(SmurfForm);
